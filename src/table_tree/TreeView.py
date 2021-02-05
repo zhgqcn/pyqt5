264e8be5a22091c31493f506117ae0376b553836
Gqq <44897668+zhgqcn@users.noreply.github.com>
2021-02-05 19:13:10 +0800

    Update TreeView.py

---
+++ src/table_tree/TreeView.py
@@ -0,0 +1,24 @@
+'''
+QTreeView 控件与系统定制模式
+QTreeWidget
+Model
+QDirModel
+
+'''
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    model = QDirModel()
+    tree = QTreeView()
+    tree.setModel(model)
+
+    tree.setWindowTitle('QTreeView')
+    tree.resize(660, 500)
+    tree.show()
+
+    sys.exit(app.exec_())
